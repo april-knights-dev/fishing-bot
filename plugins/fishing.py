ca0718a843aeb163e286c324db5c756ef6828e1d
ShingoMisawa <33051481+shngmsw@users.noreply.github.com>
2020-06-21 18:37:13 +0900

    insertとupdate実装完了

--- plugins/fishing.py
+++ plugins/fishing.py
@@ -81,8 +81,24 @@ block_kit = {
 # message.react('icon_emoji')  発言者のメッセージにリアクション(スタンプ)する
 #                               文字列中に':'はいらない
 
+@listen_to('^釣りテスト$')
+def test_fishing(message):
+
+    l_fishinfo = selectFishInfoAll(dbname)
+    l_weights = selectWeigths(dbname)
+    l = []
+    w = []
+
+    # fish_idリスト作成
+    l_fishid = [d.get('fish_id') for d in l_fishinfo]
+
+    for ret_fishid in l_fishid:
+        resultMessage = fishing(ret_fishid, l_fishinfo, user_id = message.body['user'])
+
+    message.send('全種類登録完了')
+
 @listen_to('^釣り$')
-def fishing(message):
+def listen_fishing(message):
 
     l_fishinfo = selectFishInfoAll(dbname)
     l_weights = selectWeigths(dbname)
@@ -103,35 +119,85 @@ def fishing(message):
     ret = random.choices(l_fishid, weights=w)
     ret_fishid = ret[0]
 
+    resultMessage = fishing(ret_fishid, l_fishinfo, user_id = message.body['user'])
+
+    message.send(resultMessage)
+
+def fishing(ret_fishid, l_fishinfo, user_id):
+
     # fish_idから行を取得
     fish = ""
     for row in l_fishinfo:
         if row.get('fish_id') == ret_fishid:
-            fish = row.get('fish_name') + row.get('fish_icon') + '(レア度' + str(row.get('rarity')) + '):' + row.get('comment') 
+            fishInfo = row
 
-    message.send(str(fish))
+    # 体長を範囲内でランダム生成
+    flen = 0
+    if fishInfo.get('min_length') != None:
+        flen = random.randint(fishInfo.get('min_length'),  fishInfo.get('max_length'))
+        result = fishInfo.get('fish_name') + fishInfo.get('fish_icon') + \
+                '(レア度' + str(fishInfo.get('rarity')) + ', 体長 ' + str(flen) + 'cm):' + fishInfo.get('comment') 
+    else:
+        result = fishInfo.get('fish_name') + fishInfo.get('fish_icon') + \
+                '(レア度' + str(fishInfo.get('rarity')) + '):' + fishInfo.get('comment') 
 
     # 釣果を検索
-    # 検索条件
-    user_id = message.body['user']
-    # 既に釣ってたら最小最長cm、釣った数、ポイントを更新
-    # まだ釣ってなかったら登録
+    # 検索条件    
+    l_catch_list = selectCatch(dbname, fishInfo, user_id)
+
+    if len(l_catch_list)==0:
+        # まだ釣ってなかったら登録
+        insertFishCatch(dbname, fishInfo, user_id, flen)
+    else:
+        # 既に釣ってたら最小最長cm、釣った数、ポイントを更新
+        dict_catch = l_catch_list[0]
+        before_count = dict_catch.get('count')
+        before_point = dict_catch.get('point')
+        catch_min = dict_catch.get('min_length')
+        catch_max = dict_catch.get('max_length')
+        min_length = None
+        max_length = None
+
+        if fishInfo.get('min_length') != None:
+            if flen < catch_min:
+                min_length = flen
+            else:
+                min_length = catch_min
+            if flen > catch_max:
+                max_length = flen
+            else:
+                max_length = catch_max
+        else:
+            min_length = None
+            min_length = None
+        updateFishCatch(dbname, fishInfo, user_id, min_length, max_length, before_count, before_point)
+
+    return str(result)
 
 @listen_to('^底びき網漁$')
 def fishingAll(message):
  
-    resultList = selectFishInfoAll(dbname)
-    weights = selectWeigths(dbname)
+    l_fishinfo = selectFishInfoAll(dbname)
+    l_weights = selectWeigths(dbname)
     l = []
     w = []
 
-    for num in range(10000):
-        ret = random.choices(l, weights=w)
-        fish_count[ret[0]] = fish_count[ret[0]] + 1
+    # fish_idリスト作成
+    l_fishid = [d.get('fish_id') for d in l_fishinfo]
+        # レア度リスト取得
+    for row in l_fishinfo:
+        rarity = row.get('rarity')
+        # レア度を％に変換する
+        for row_w in l_weights:
+            if row_w.get('rarity') == rarity:
+                w.append(row_w.get('weights'))
 
-    for k, v in fish_count.items():
-        message.send(f"{k}は{v}匹\n")
+    for num in range(100):
+        ret = random.choices(l_fishid, weights=w)
+        ret_fishid = ret[0]
+        resultMessage = fishing(ret_fishid, l_fishinfo, user_id = message.body['user'])
 
+    message.send('10000匹釣ったで')
     # # APIを使った投稿
     # client.chat_postMessage(
     #     channel='#tmp_bot放牧部屋',
@@ -180,7 +246,7 @@ def selectWeigths(dbName):
 
     return resultList
 
-def selectCatch(dbName, fishInfo, userInfo):
+def selectCatch(dbName, fishInfo, userId):
     conn = sqlite3.connect(dbName)
     # row_factoryの変更(dict_factoryに変更)
     conn.row_factory = dict_factory
@@ -188,7 +254,7 @@ def selectCatch(dbName, fishInfo, userInfo):
     c = conn.cursor()
 
     sql = "select * from fish_catch where fish_id =? and angler_id=?"
-    c.execute(sql, [fishInfo.get('fish_id'), userInfo('user')])
+    c.execute(sql, [fishInfo.get('fish_id'), userId])
 
     resultList = c.fetchall()
 
@@ -197,17 +263,61 @@ def selectCatch(dbName, fishInfo, userInfo):
 
     return resultList
 
-def insertFishCatch(dbName, fishInfo, userInfo):
+def insertFishCatch(dbName, fishInfo, userId, length):
+    conn = sqlite3.connect(dbName)
+    c = conn.cursor()
+    try:
+
+        sql = "INSERT INTO fish_catch (fish_id, angler_id, min_length, max_length, count, point) VALUES (?, ?, ?, ?, ?, ?)"
+        # 1匹につきレア度に応じて以下のポイントを付与
+        # 1 ** 5 = 1
+        # 2 ** 5 = 32
+        # 3 ** 5 = 243
+        # 4 ** 5 = 1024
+        # 5 ** 5 = 3125
+        rarity = fishInfo.get('rarity') 
+        if rarity <= 5:
+            point = rarity ** 5
+        else:
+            point = rarity * 2
+
+        c.execute(sql, 
+            [fishInfo.get('fish_id'), userId, length, length, 1, point])
+        c.close()
+    except sqlite3.IntegrityError as e:
+        print(e)
+        
+    conn.commit()
+    conn.close()
+
+def updateFishCatch(dbName, fishInfo, userId, min_length, max_length, before_count, before_point):
     conn = sqlite3.connect(dbName)
     # row_factoryの変更(dict_factoryに変更)
     c = conn.cursor()
+    try:
 
-    sql = "INSERT INTO fish_catch (fish_id, angler_id, min_length, max_length, count, point) VALUES (?, ?, ?, ?, ?, ?);"
-    c.execute(sql)
+        sql = "UPDATE fish_catch SET min_length=?, max_length=?, count=?, point=? where fish_id=? and angler_id=?"
+        count = before_count + 1
+        # 1匹につきレア度に応じて以下のポイントを付与
+        # 1 ** 5 = 1
+        # 2 ** 5 = 32
+        # 3 ** 5 = 243
+        # 4 ** 5 = 1024
+        # 5 ** 5 = 3125
+        rarity = fishInfo.get('rarity') 
+        if rarity <= 5:
+            point = count * (fishInfo.get('rarity') ** 5)
+        else:
+            point = count * (fishInfo.get('rarity') * 2)
 
-    resultList = c.fetchall()
+        c.execute(sql, 
+            [min_length, max_length, count, point, fishInfo.get('fish_id'), userId])
+        c.close()
 
-    c.close()
+    except sqlite3.IntegrityError as e:
+        print(e)
+        
+    conn.commit()
     conn.close()
 
 # dict_factoryの定義
@@ -223,5 +333,3 @@ def is_int(s):
         return True
     except ValueError:
         return False
-
-# fishing("")
