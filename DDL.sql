3b171ca547b2aa39435de40b1b8c4d5808621552
ShingoMisawa <33051481+shngmsw@users.noreply.github.com>
2020-06-26 11:21:51 +0900

    DDL初回コミット

---
+++ DDL.sql
@@ -0,0 +1,118 @@
+-- テーブル作成
+create table fish_info(
+    fish_id VARCHAR (20) primary key,
+    fish_name VARCHAR (20),
+    fish_icon TEXT,
+    min_length integer,
+    max_length integer,
+    rarity integer,
+    comment TEXT,
+    created_at timestamp not null default current_timestamp,
+    updated_at timestamp not null default current_timestamp
+);
+
+create table fish_catch(
+    fish_id VARCHAR (20),
+    angler_id VARCHAR (20),
+    min_length integer,
+    max_length integer,
+    count integer,
+    point integer,
+    created_at timestamp not null default current_timestamp,
+    updated_at timestamp not null default current_timestamp,
+    CONSTRAINT fish_catch_pk PRIMARY KEY(fish_id, angler_id)
+);
+
+drop table angler_ranking;
+
+create table angler_ranking(
+    angler_id VARCHAR (20),
+    total_point integer,
+    created_at timestamp not null default current_timestamp,
+    updated_at timestamp not null default current_timestamp,
+    constraint upsert_pk primary key(angler_id)
+);
+
+create table weights(
+    rarity integer primary key,
+    weights real,
+    created_at timestamp not null default current_timestamp,
+    updated_at timestamp not null default current_timestamp
+);
+
+-- 更新日時のトリガー作成
+create function set_update_time() returns opaque as '
+  begin
+    new.updated_at := ''now'';
+    return new;
+  end;
+' language 'plpgsql';
+
+create trigger update_tri before
+update
+    on fish_info for each row execute procedure set_update_time();
+
+create trigger update_tri before
+update
+    on fish_catch for each row execute procedure set_update_time();
+
+create trigger update_tri before
+update
+    on angler_ranking for each row execute procedure set_update_time();
+
+create trigger update_tri before
+update
+    on weights for each row execute procedure set_update_time();
+
+DROP function angler_ranking_trigger_function CASCADE;
+
+-- 集計トリガー作成
+DROP function angler_ranking_trigger_function CASCADE;
+CREATE
+OR  REPLACE FUNCTION angler_ranking_trigger_function(
+    ) RETURNS TRIGGER AS $BODY$ BEGIN
+    -- TG_TABLE_NAME :name of the table that caused the trigger invocation
+    IF(
+        TG_TABLE_NAME = 'fish_catch'
+    ) THEN
+    --TG_OP : operation the trigger was fired
+    IF(TG_OP = 'INSERT') OR (TG_OP = 'UPDATE') THEN
+        INSERT INTO angler_ranking(angler_id, total_point)
+        (
+            SELECT
+                angler_id,
+                SUM(point)
+            FROM
+                fish_catch
+            GROUP BY
+                angler_id
+        )
+        ON  CONFLICT
+        ON  CONSTRAINT upsert_pk DO
+            UPDATE
+            SET
+                total_point = (
+                    SELECT
+                        SUM(point)
+                    FROM
+                        fish_catch
+                    WHERE
+                        angler_id = OLD.angler_id
+                    GROUP BY
+                        angler_id
+                ),
+                updated_at = NOW()
+            WHERE
+                angler_ranking.angler_id = OLD.angler_id
+        ;
+RETURN NEW;
+END IF;
+RETURN NULL;
+END IF;
+END;
+$BODY$ LANGUAGE plpgsql VOLATILE COST 100;
+
+ALTER FUNCTION angler_ranking_trigger_function() OWNER TO postgres;
+
+CREATE TRIGGER angler_ranking_trigger BEFORE INSERT OR UPDATE
+    ON  fish_catch FOR EACH ROW EXECUTE PROCEDURE angler_ranking_trigger_function();
