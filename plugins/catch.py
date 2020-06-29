e70b1c67f57a71423b971673f3cb22ebc7751d9a
mirarex <kd1242893@st.kobedenshi.ac.jp>
2020-06-29 23:33:51 +0900

    conflict修正。ボケなくてもいい

--- plugins/catch.py
+++ plugins/catch.py
@@ -1,17 +1,11 @@
 # coding: utf-8
 from slack import WebClient
-<<<<<<< HEAD
 
-=======
->>>>>>> origin/master
 from slack.errors import SlackApiError
 from slackbot.bot import respond_to     # @botname: で反応するデコーダ
 from slackbot.bot import listen_to      # チャネル内発言で反応するデコーダ
 from slackbot.bot import default_reply  # 該当する応答がない場合に反応するデコーダ
-<<<<<<< HEAD
 
-=======
->>>>>>> origin/master
 from psycopg2.extras import DictCursor
 import os
 import requests
@@ -255,16 +249,3 @@ def fish_catch(message):
     except AttributeError:
         send_text = "まだ登録されてませんよ？"
         message.send(send_text)
-<<<<<<< HEAD
-
-import requests
-import urllib.request as req
-import sys
-import json
-
-@listen_to('釣果')
-def listen_func(message):
-    message.send('まだ準備中（平田くんが頑張ってます）')      # ただの投稿
-=======
-    
->>>>>>> origin/master
