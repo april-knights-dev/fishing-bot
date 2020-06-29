9dae065cfe43b8755f99ac22a4e5a843d49c368e
mirarex <kd1242893@st.kobedenshi.ac.jp>
2020-06-29 22:44:34 +0900

    Initial Commit

--- plugins/catch.py
+++ plugins/catch.py
@@ -1,14 +1,12 @@
-<<<<<<< HEAD
 # coding: utf-8
 
 from slack import WebClient
-=======
->>>>>>> origin/master
+
 from slack.errors import SlackApiError
 from slackbot.bot import respond_to     # @botname: で反応するデコーダ
 from slackbot.bot import listen_to      # チャネル内発言で反応するデコーダ
 from slackbot.bot import default_reply  # 該当する応答がない場合に反応するデコーダ
-<<<<<<< HEAD
+
 from psycopg2.extras import DictCursor
 import os
 import requests
@@ -252,8 +250,7 @@ def fish_catch(message):
     except AttributeError:
         send_text = "まだ登録されてませんよ？"
         message.send(send_text)
-    
-=======
+
 import requests
 import urllib.request as req
 import sys
@@ -262,5 +259,3 @@ import json
 @listen_to('釣果')
 def listen_func(message):
     message.send('まだ準備中（平田くんが頑張ってます）')      # ただの投稿
-    # message.reply('君だね？')
->>>>>>> origin/master
